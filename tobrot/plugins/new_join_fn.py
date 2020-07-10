5d89d2fa7524ef15d3f3fe7b019c09cd80e46385
Shubham Bagde <64890390+thetechboy3@users.noreply.github.com>
2020-07-10 22:36:17 +0530

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -38,7 +38,7 @@ async def help_message_f(client, message):
     #message_id = 99
     # display the /help
     
-    await message.reply_text("""Read pinned message: <a href="https://t.me/cooboysskleech/128">Pinned Message</a>""", disable_web_page_preview=True)
+    await message.reply_text("""Read pinned message: <a href="https://t.me/thetechboyleech/21">Pinned Message</a>""", disable_web_page_preview=True)
 
 
 async def rename_message_f(client, message):
